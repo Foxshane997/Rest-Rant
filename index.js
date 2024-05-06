5fbd77e4ff4075af360270aabef6c3fd43a79531
“Foxshane997” <Foxshane997@gmail.com>
2024-05-05 23:30:32 -0700

    Adding 404 page view

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get('/', (req, res) => {
 
 // 404 Page
 app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page</h1>')
+    res.render('error404')
 })
 
 // Always Have this last.
