34769ce9da69104f2818707d60a7c60fa4b5bd41
“Foxshane997” <Foxshane997@gmail.com>
2024-05-06 21:34:07 -0700

    added 2x images to index

--- controllers/places.js
+++ controllers/places.js
@@ -8,13 +8,13 @@ router.get('/', (req, res) => {
         city: 'Seattle',
         state: 'WA',
         cuisines: 'Thai, Pan-Asian',
-        pic: '/public/images/emy-XoByiBymX20-unsplash.jpg'
+        pic: '/images/emy-XoByiBymX20-unsplash.jpg'
       }, {
         name: 'Coding Cat Cafe',
         city: 'Phoenix',
         state: 'AZ',
         cuisines: 'Coffee, Bakery',
-        pic: '/public/images/andriyko-podilnyk-RCfi7vgJjUY-unsplash (1).jpg'
+        pic: '/images/andriyko-podilnyk-RCfi7vgJjUY-unsplash (1).jpg'
       }]      
     res.render('places/index', {places})
   })
