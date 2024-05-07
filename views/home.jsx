0cdb1de6ef38dfeffa03960fdc2168e5ae134ef5
“Foxshane997” <Foxshane997@gmail.com>
2024-05-06 20:27:15 -0700

    added images to home & 404 pages

--- views/home.jsx
+++ views/home.jsx
@@ -10,6 +10,12 @@ function home () {
         </head>
           <main>
               <h1>HOME</h1>
+              <div>
+                <img src="/images/anh-nguyen-kcA-c3f_3FE-unsplash.jpg" alt="Bibimbap image" />
+                <div>
+                  Photo by <a href="https://images.unsplash.com/photo-1546069901-ba9599a7e63c?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">Anh Nguyen</a> on <a href="UNSPLASH_LINK">Unsplash</a>
+                </div>
+              </div>
               <a href="/places">
                 <button className="btn-primary">Places Page</button>
               </a>
@@ -19,4 +25,5 @@ function home () {
   }  
 
 module.exports = home
+
 
