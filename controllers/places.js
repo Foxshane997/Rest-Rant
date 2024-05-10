185b9f43dd60f5790af3bc17e513a6dd535cd20c
“Foxshane997” <Foxshane997@gmail.com>
2024-05-09 23:00:49 -0700

    Added the fix for the issue, Thank you John!

--- controllers/places.js
+++ controllers/places.js
@@ -12,9 +12,12 @@ router.get('/new', (req, res) => {
   res.render('places/new')
 })
 
-router.get('/:id', (req, res) => {
-  res.render('places/show')
-})
+router.get("/:id", (req, res) => {
+  let id = Number(req.params.id);
+  if (isNaN(id)) { res.render("error404");
+  } else if (!places[id]) { res.render("error404");
+  } else
+  { res.render("places/show", { place: places[id], id: id }); } });
 
 // Edit route
 // Didn't say where to put this in the instructions.
