b65cbc0f209d95e58a407b9739a00f6a122050ba
“Foxshane997” <Foxshane997@gmail.com>
2024-05-09 23:07:27 -0700

    Added the delete feature

--- controllers/places.js
+++ controllers/places.js
@@ -34,6 +34,20 @@ router.get('/:id/edit', (req, res) => {
   }
 })
 
+router.delete('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    places.splice(id, 1)
+    res.redirect('/places')
+  }
+})
+
 // POST /places
 router.post('/', (req, res) => {
   // console.log(req.body);
