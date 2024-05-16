9af09897e430566c03ed861d3d9a1bd8e9763e50
“Foxshane997” <Foxshane997@gmail.com>
2024-05-15 21:15:26 -0700

    updating

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -1,12 +1,12 @@
 const React = require("react");
 const Def = require("../default");
 
-function new_form() {
+function new_form(data) {
   return (
     <Def>
       <main>
         <h1>Add a New Place</h1>
-        <form action="/places" method="post">
+        <form action="/places" method="POST">
           <div className="form-columns">
             <label htmlFor="name">Place Name:</label>
             <input id="name" name="name" required />
@@ -25,17 +25,21 @@ function new_form() {
           </div>
           <div className="form-group">
             <label for="founded">Founded Year</label>
-            <input className="form-control" id="founded" name="founded" />
+            <input 
+            type="number"
+            className="form-control" 
+            id="founded" 
+            name="founded"
+            value={new Date().getFullYear()} />
           </div>
           <div className="form-columns">
             <label htmlFor="cuisines">Cuisines:</label>
-            <input id="cuisines" name="cuisines" />
+            <input id="cuisines" name="cuisines" required />
           </div>
           <input
             type="submit"
             value="Add Place"
             className="btn"
-            required
           />
         </form>
       </main>
