9ae0eea2ddb945eef73e73989f154a936aaef10a
“Foxshane997” <Foxshane997@gmail.com>
2024-05-18 14:32:47 -0700

    Added abilty to submit the comment made

--- controllers/places.js
+++ controllers/places.js
@@ -44,10 +44,34 @@ router.get('/:id', (req, res) => {
   })
 })
 
+// Create Comment
+router.post('/:id/comment', (req, res) => {
+  console.log(req.body)
+  db.Place.findById(req.params.id)
+  .then(place => {
+      db.Comment.create(req.body)
+      .then(comment => {
+          place.comments.push(comment.id)
+          place.save()
+          .then(() => {
+              res.redirect(`/places/${req.params.id}`)
+          })
+      })
+      .catch(err => {
+          res.render('error404')
+      })
+  })
+  .catch(err => {
+      res.render('error404')
+  })
+})
+
+// Places Get id Stub 
 router.get('/:id', (req, res) => {
   res.send('GET /places/:id stub')
 })
 
+// Places put id Stub
 router.put('/:id', (req, res) => {
   res.send('PUT /places/:id stub')
 })
@@ -67,10 +91,12 @@ router.post('/:id/rant', (req, res) => {
   res.send('GET /places/:id/rant stub')
 })
 
+// Delete Stub
 router.delete('/:id/rant/:rantId', (req, res) => {
     res.send('GET /places/:id/rant/:rantId stub')
 })
 
+// Export
 module.exports = router
 
 // // The controller area
