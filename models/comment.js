ae2047e92fffccc224356cc1c3dc4b53581226b8
“Foxshane997” <Foxshane997@gmail.com>
2024-05-18 12:47:09 -0700

    Added comments file & schema. edited palces.js schema

---
+++ models/comment.js
@@ -0,0 +1,14 @@
+// dependencies 
+const mongoose = require('mongoose')
+
+// Schema
+let commentSchema = new mongoose.Schema({
+    author: { type: String, default: 'Anonymous' },
+    rant: { type: Boolean, default: false },
+    stars: { type: Number, required: true },
+    content: { type: String, default: '' },
+})
+
+// export
+module.exports = mongoose.model('Comment', commentSchema)
+module.exports.Comment = require('./comment')
